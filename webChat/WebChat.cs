b0bb740df33b992298fe2e67fb05c36ad6ca07f4
sergio <sergio.gonzalez@tchlabs.eu>
2024-06-29 13:16:46 +0200

    implemetacion de header en el chat

--- webChat/WebChat.cs
+++ webChat/WebChat.cs
@@ -1,27 +1,38 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using backend_tfg.interfaces;
 using backend_tfg.repositorios;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.IdentityModel.Tokens;
+using MongoDB.Driver.Core.Authentication;
 
 public class WebChat : Hub
 {
     private static ConcurrentDictionary<string, string> UsuariosConectados = new ConcurrentDictionary<string, string>();
     private readonly IChatRepositorio _chatRepositorio;
+    private IConfiguration _config;
 
-    public WebChat(IChatRepositorio chatRepositorio){
+    public WebChat(IChatRepositorio chatRepositorio, IConfiguration config)
+    {
         _chatRepositorio = chatRepositorio;
+        _config = config;
 
     }
 
-     public override async Task OnConnectedAsync()
+    public override async Task OnConnectedAsync()
     {
         var userName = Context.GetHttpContext().Request.Query["user"].ToString();
-        var estado = UsuariosConectados.AddOrUpdate(userName, Context.ConnectionId, (key, oldValue) => Context.ConnectionId);        await Clients.All.SendAsync("UserConnected", userName);
-        await base.OnConnectedAsync();
+        var token = Context.GetHttpContext().Request.Query["token"].ToString();
+        if (userName is not null && token is not null && ValidateToken(token))
+        {
+            var estado = UsuariosConectados.AddOrUpdate(userName, Context.ConnectionId, (key, oldValue) => Context.ConnectionId); await Clients.All.SendAsync("UserConnected", userName);
+            await base.OnConnectedAsync();
+        }
     }
 
     public override async Task OnDisconnectedAsync(Exception exception)
@@ -39,6 +50,33 @@ public class WebChat : Hub
         {
             await Clients.Client(connectionId).SendAsync("mensajePrivado", message);
             var chat = await this._chatRepositorio.postMessageUsers(message);
+        }
+    }
+
+    private bool ValidateToken(string token)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.UTF8.GetBytes(_config["JWT_SECRET"]);
+
+        try
+        {
+            tokenHandler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = _config["JWT_ISSUER"],
+                ValidAudience = _config["JWT_AUDIENCE"],
+                IssuerSigningKey = new SymmetricSecurityKey(key)
+            }, out SecurityToken validatedToken);
+
+            return true;
+        }
+        catch
+        {
+            // Token inválido
+            return false;
         }
     }
 }
